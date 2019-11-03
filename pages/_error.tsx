7c20f719ffe76409272b6e58e8e0ec8b9d5b3dd9
Sviatoslav Nazar <sv9toslav.n@gmail.com>
2019-11-04 01:59:29 +0200

    Nextjs setup

---
+++ pages/_error.tsx
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react';
+import Index from './index';
+import '../css/index.scss';
+
+class Error extends React.Component<{ statusCode: number }> {
+  static getInitialProps({ res, err }: any) {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+    return { statusCode };
+  }
+
+  getResponse(statusCode: number) {
+    switch (statusCode) {
+      case 404:
+        console.log('Rendering home');
+        return <Index />;
+      case 500:
+        return <p>An error occurred on the server</p>;
+      default:
+        return <p>An error occurred on client</p>;
+    }
+  }
+
+  render() {
+    return <Fragment>{this.getResponse(this.props.statusCode)}</Fragment>;
+  }
+}
+
+export default Error;
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import '../css/index.scss';
+import HelloWorld from '../components/HelloWorld';
+
+const Index = () => {
+  return (
+    <div className="page">
+      <HelloWorld />
+    </div>
+  );
+};
+
+export default Index;
